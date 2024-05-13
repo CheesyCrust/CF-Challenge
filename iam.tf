6fba0b5dd27dae9dad93d392c2b12c0263f6d5c6
CheesyCrust <crswain94@yahoo.com>
2024-05-13 05:32:02 -0400

    Updated IAM/S3 bucket changes

--- iam.tf
+++ iam.tf
@@ -48,29 +48,29 @@ resource "aws_iam_instance_profile" "POCallowSSM-Profile" {
     role = aws_iam_role.POCallowSSM.name
 }
 
-# resource "aws_iam_role" "POC-ALB-Allow" {
-#     name = "POC-ALB-Allow"
-#     assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
+resource "aws_iam_role" "POC-ALB-Allow" {
+    name = "POC-ALB-Allow"
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "ssm_policy_attachment-alb" {
-#     role = aws_iam_role.POC-ALB-Allow.name
-#     policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-# }
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment-alb" {
+    role = aws_iam_role.POC-ALB-Allow.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
-# resource "aws_iam_instance_profile" "POC-ALB-Allow-Profile" {
-#     name = "POC-ALB-Allow"
-#     role = aws_iam_role.POC-ALB-Allow.name
-# }
+resource "aws_iam_instance_profile" "POC-ALB-Allow-Profile" {
+    name = "POC-ALB-Allow"
+    role = aws_iam_role.POC-ALB-Allow.name
+}
--- s3.tf
+++ s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "POC-Images" {
     bucket = "cswain-challenge-images"
 }
 
-resource "aws_s3_object" "POC-arhive" {
+resource "aws_s3_object" "POC-archive" {
     bucket = aws_s3_bucket.POC-Images.id
     key = "/archive/"
 }
@@ -14,4 +14,74 @@ resource "aws_s3_object" "POC-arhive" {
 resource "aws_s3_object" "POC-memes" {
     bucket = aws_s3_bucket.POC-Images.id
     key = "/memes/"
+}
+
+resource "aws_s3_bucket" "POC-Logs" {
+    bucket = "cswain-challenge-logs"
+}
+
+resource "aws_s3_object" "POC-active" {
+    bucket = aws_s3_bucket.POC-Logs.id
+    key = "/active/"
+}
+
+resource "aws_s3_object" "POC-inactive" {
+    bucket = aws_s3_bucket.POC-Logs.id
+    key = "/inactive/"
+}
+
+
+###########
+#Policies
+###########
+
+resource "aws_s3_bucket_lifecycle_configuration" "POC-Glacier-Move-images" {
+    depends_on = [aws_s3_bucket.POC-Images]
+    bucket = aws_s3_bucket.POC-Images.id
+
+    rule {
+        id = "90Days-Storage"
+
+        filter {
+            prefix = "memes/"
+        }
+
+        transition {
+            days = 90
+            storage_class = "GLACIER"
+        }
+    status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "POC-Glacier-Move-ActiveLogs" {
+    depends_on = [aws_s3_bucket.POC-Logs]
+    bucket = aws_s3_bucket.POC-Logs.id
+
+        rule {
+            id = "90Days-Storage"
+
+            filter {
+                prefix = "active/"
+            }
+
+            transition {
+                days = 90
+                storage_class = "GLACIER"
+            }
+        status = "Enabled"
+        }
+
+        rule {
+            id = "90Days-Deletion"
+
+            filter {
+                prefix = "inactive/"
+            }
+
+            expiration {
+                days = 90
+            }
+        status = "Enabled"
+        }
 }
